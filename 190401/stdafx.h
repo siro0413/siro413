7aede53bb1b9a823ff3fbf287bcd2f4bf9cbb4ab
siro0413 <48557104+siro0413@users.noreply.github.com>
2019-04-01 18:03:00 +0900

    Add files via upload

---
+++ 190401/stdafx.h
@@ -0,0 +1,18 @@
+#pragma once
+//라이브러리 추가 window 멀티미디어.lib
+#pragma comment(lib,"winmm.lib")
+
+#include <Windows.h>
+#include <assert.h> //단언문 이상하다싶으면 코드자체를 중단
+
+#include <iostream>
+#include <random>
+#include <string>
+#include <vector>
+#include <map>
+
+
+#include "./system/definition.h"
+#include "./system/Renderer.h"
+#include "./system/Collision.h"
+#include "system/bulletm.h"
---
+++ 190401/system/Collision.cpp
@@ -0,0 +1,61 @@
+#include "stdafx.h"
+#include "Collision.h"
+#include "../Gameobj/Player.h"
+#include "../Gameobj/Monster.h"
+
+const bool Collision::IsInside(Player * player, Monster * monster)
+{
+    //if (player->pos.x == monster->pos.x && player->pos.x == monster->pos.x)
+    //    return true;
+
+    //bool bCheck = false;
+    //bCheck |= player->pos.x == monster->pos.x;
+    //bCheck |= player->pos.y == monster->pos.y;
+
+    if (player->pos == monster->pos)
+        return true;
+
+    return false;
+}
+
+//+ - * /
+//+= -= *= /=
+//== !=
+
+
+const bool Collision::IsIntersect(Player * player, Monster * monster)
+{
+    //Up
+    bool bCheck = true;
+    bCheck &= player->pos.x == monster->pos.x;
+    bCheck &= player->pos.y - 1 == monster->pos.y;
+    
+    if (bCheck)
+        return true;
+
+    //Down
+    bCheck = true;
+    bCheck &= player->pos.x == monster->pos.x;
+    bCheck &= player->pos.y + 1 == monster->pos.y;
+
+    if (bCheck)
+        return true;
+
+    //Left
+    bCheck = true;
+    bCheck &= player->pos.x - 1 == monster->pos.x;
+    bCheck &= player->pos.y == monster->pos.y;
+
+    if (bCheck)
+        return true;
+
+    //Right
+    bCheck = true;
+    bCheck &= player->pos.x + 1 == monster->pos.x;
+    bCheck &= player->pos.y == monster->pos.y;
+
+    if (bCheck)
+        return true;
+
+    return false;
+}
