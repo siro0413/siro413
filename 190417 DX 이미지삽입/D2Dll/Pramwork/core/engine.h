a2498d66aa56ed03c7e658284c5d9bc2ccbf7669
siro0413 <48557104+siro0413@users.noreply.github.com>
2019-04-17 17:53:26 +0900

    Add files via upload

---
+++ 190417 DX 이미지삽입/D2Dll/Pramwork/core/engine.h
@@ -0,0 +1,71 @@
+#pragma once
+struct  VertexColor
+{
+	D3DXVECTOR3 position;
+	D3DXCOLOR color;
+};
+
+struct  VertexTexture
+{
+	D3DXVECTOR3 position;
+	D3DXVECTOR2 uv;
+};
+//시작점 모든걸 다가지고있을예정 (main급)
+class Engine final
+{
+public:
+	Engine();
+	~Engine();
+
+	auto GetContext() const -> class Context* { return context; }
+
+
+	void Update();
+	void Render();
+
+
+private:
+	class Context* context;
+	class Graphics* graphics;
+
+	//===========================//
+	VertexTexture* vertices;
+	//↓IA에 들어가기위해 변환하는 함수
+	ID3D11Buffer* vertexBuffer;
+
+	//IndexBuffer
+	uint* indices;
+	ID3D11Buffer* IndexBuffer;
+
+	//Shader
+	ID3D11VertexShader* vertexShader;
+	ID3DBlob* vsBlob;
+	
+	//Blob shader가 제대로 컴파일이되었는지 확인해주는함수
+	ID3D11PixelShader* pixelShader;
+	ID3DBlob* psBlob;
+	
+	//틀
+	ID3D11InputLayout* inputLayout;
+
+	//공간변화는 shader
+	D3DXMATRIX world;
+	D3DXMATRIX view;
+	D3DXMATRIX proj;
+
+	struct TransformData
+	{
+		D3DXMATRIX World;
+		D3DXMATRIX View;
+		D3DXMATRIX Proj;
+	}data;
+	//buffer로쓰면 헷갈리니 c= 상수를 붙임
+	ID3D11Buffer* cbuffer;
+
+	ID3D11RasterizerState* rsState;
+
+	ID3D11ShaderResourceView* srv;
+	//ID3D11Texture2D -> 포켓몬 이브이같은애 여러가지로 변형이가능
+	
+	ID3D11BlendState* blendState; //OM부분 혼합
+};
---
+++ 190417 DX 이미지삽입/D2Dll/stdafx.h
@@ -0,0 +1,52 @@
+#pragma once
+
+#ifdef _DEBUG
+#pragma comment(linker,"/entry:WinMainCRTStartup /subsystem:console")
+
+
+#endif // _DEBUG 콘솔을 사용하게해주는 정리되어있는 명령어
+
+
+//window
+#include <Windows.h>
+#include <assert.h>
+//STL
+#include <typeinfo>
+#include <iostream>
+#include <vector>
+typedef unsigned int uint;
+typedef unsigned long ulong;
+
+
+//DirectX
+#include <dxgi1_2.h>
+#include <d3dcommon.h>
+#include <d3dcompiler.h>
+#include <d3d11shader.h>
+#include <d3d11.h>
+#include <D3DX10math.h>
+#include <D3DX11async.h>
+
+
+#pragma comment(lib, "dxgi.lib")
+#pragma comment(lib, "d3d11.lib")
+#pragma comment(lib, "d3dx9.lib")
+#pragma comment(lib, "d3dx10.lib")
+#pragma comment(lib, "d3dx11.lib")
+#pragma comment(lib, "d3dcompiler.lib")
+#pragma comment(lib, "dxguid.lib")
+
+
+//Macro Funtion
+#define SAFE_RELEASE(p)         { if (p) { (p)->Release(); (p) = nullptr; } }
+#define SAFE_DELETE(p)          { if (p) { delete (p); (p) = nullptr; } }
+#define SAFE_DELETE_ARRAY(p)    { if (p) { delete[] (p); (p) = nullptr; } }
+
+
+
+//Main system
+#include "Pramwork/core/Settings.h"
+#include "Pramwork/core/Context.h"
+
+//subsystem
+#include "Pramwork/core/subsystem/Graphics.h"
