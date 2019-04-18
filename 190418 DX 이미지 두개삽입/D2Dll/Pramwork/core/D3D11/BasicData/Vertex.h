191d79f95f8b2159f130c497b76550941af087a2
siro0413 <48557104+siro0413@users.noreply.github.com>
2019-04-18 21:26:46 +0900

    Add files via upload

---
+++ 190418 DX 이미지 두개삽입/D2Dll/Pramwork/core/D3D11/BasicData/Vertex.h
@@ -0,0 +1,39 @@
+#pragma once
+
+struct  VertexColor
+{
+	VertexColor()
+		:position(0, 0, 0)
+		, color(0, 0, 0, 0)
+	{}
+
+	VertexColor(const D3DXVECTOR3& position, const D3DXVECTOR3& color)
+		: position(position)
+		, color(color)
+	{}
+
+	D3DXVECTOR3 position;
+	D3DXCOLOR color;
+
+	static D3D11_INPUT_ELEMENT_DESC descs[];
+	static const uint count = 2;
+
+};
+
+struct  VertexTexture
+{
+	VertexTexture()
+		:position(0, 0, 0)
+		, uv(0, 0)
+	{}
+
+	VertexTexture(const D3DXVECTOR3& position, const D3DXVECTOR2& uv)
+		: position(position)
+		, uv(uv)
+	{}
+	D3DXVECTOR3 position;
+	D3DXVECTOR2 uv;
+
+	static D3D11_INPUT_ELEMENT_DESC descs[];
+	static const uint count = 2;
+};
---
+++ 190418 DX 이미지 두개삽입/D2Dll/TextureCode.hlsl
@@ -0,0 +1,52 @@
+cbuffer TansformBuffer : register(b0)
+{
+    //row_major 열우선을 행우선으로 바꿔라
+   matrix World;
+   matrix View;
+   matrix Proj;
+};
+
+struct VertexInput
+{
+    float4 Position : POSITION0;
+    float2 Uv : TEXCORD0;
+};
+
+struct PixelInput
+{
+    //SV = 컴퓨터에 찍을 위치지정
+    float4 Position : SV_POSITION0;
+	float2 Uv : TEXCORD0;
+};
+
+PixelInput VS(VertexInput input)
+{
+
+    PixelInput output;
+    output.Position = mul(input.Position, World);
+    output.Position = mul(output.Position, View);
+    output.Position = mul(output.Position, Proj);
+    output.Uv = input.Uv;
+
+    return output;
+
+}
+
+Texture2D SourceTexture1 : register(t0);
+Texture2D SourceTexture2 : register(t1);
+
+
+SamplerState samp: register(s0);
+
+//OM에들어가는 도화지 총8장중 어디에쓸건지
+float4 PS(PixelInput input) :SV_Target
+{
+    float4 color = 0;
+    if(input.Uv.x<1.0f)
+        color = SourceTexture1.Sample(samp, input.Uv);
+    else
+        color = SourceTexture2.Sample(samp, float2(input.Uv.x - 1.0f, input.Uv.y));
+
+
+    return color;
+}
